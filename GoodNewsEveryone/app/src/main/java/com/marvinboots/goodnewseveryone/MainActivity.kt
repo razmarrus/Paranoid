87daa03a70f5ee16627c427d7caa9623f0c9977a
razmarrus <mrazumeyev@gmail.com>
2019-12-24 06:06:57 +0300

    Basic RSS Reader (lab4)

---
+++ GoodNewsEveryone/app/src/main/java/com/marvinboots/goodnewseveryone/MainActivity.kt
@@ -0,0 +1,216 @@
+package com.marvinboots.goodnewseveryone
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.app.ActivityCompat
+import java.util.ArrayList
+import java.util.HashMap
+
+
+import android.app.ProgressDialog
+import android.widget.ProgressBar
+import android.os.AsyncTask
+import android.util.Log
+import android.widget.Toast
+//import android.support.design.widget.FloatingActionButton
+//import android.support.design.widget.Snackbar
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import org.xml.sax.SAXException
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.parsers.ParserConfigurationException
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mRecyclerView: RecyclerView //? = null
+    private lateinit var mAdapter: RecycleViewAdapter //? = null
+    private val resultItems = ArrayList<HashMap<String, String>>()
+    private val mFeedUrl = "http://feeds.feedburner.com/techcrunch/android?format=xml"
+    private lateinit var progressDialog: ProgressBar //= null
+    private var isLoading: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED)
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.INTERNET),
+                    1
+                )
+
+        }
+        /*val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener(View.OnClickListener { view ->
+            Snackbar.make(view, "Rss Feed Url: $mFeedUrl", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        })*/
+
+        mRecyclerView = findViewById(R.id.recycler_view) as RecyclerView
+
+        val mLayoutManager = LinearLayoutManager(this)
+        mRecyclerView.setLayoutManager(mLayoutManager)
+
+        mAdapter = RecycleViewAdapter(this, resultItems)
+        mRecyclerView.setAdapter(mAdapter)
+
+        getDataFromWeb()
+
+    }
+
+    inner class GetRssClass(internal var mUrl: String) :
+        AsyncTask<Void, Void, ArrayList<HashMap<String, String>>>()
+    {
+        init{
+            mUrl = mFeedUrl
+        }
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showProgressDialog()
+        }
+
+        override fun doInBackground(vararg voids: Void): ArrayList<HashMap<String, String>>? {
+            var result = ArrayList<HashMap<String, String>>()
+            try {
+                val url = URL(mUrl)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.connectTimeout = 15000
+                val responseCode = connection.responseCode
+                println(responseCode)
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val inputStream = connection.inputStream
+                    result = parseXML(inputStream)
+                } else {
+                    return null
+                }
+            } catch (e: Exception) {
+                Log.d("Exception", e.message)
+                return null
+            }
+
+            return result
+        }
+
+
+        override fun onPostExecute(result: ArrayList<HashMap<String, String>>?) {
+            super.onPostExecute(result)
+            hideProgressDialog()
+            if (result == null) {
+                showToast("Please check your connection and try again.")
+            } else {
+                val before = resultItems.size
+                resultItems.clear()
+                resultItems.addAll(result)
+                mAdapter.notifyItemRangeInserted(before, result.size)
+                mRecyclerView.invalidate()
+            }
+        }
+
+        @Throws(ParserConfigurationException::class, IOException::class, SAXException::class)
+        private fun parseXML(inputStream: InputStream): ArrayList<HashMap<String, String>> {
+
+            val documentBuilderFactory = DocumentBuilderFactory.newInstance()
+            val documentBuilder = documentBuilderFactory.newDocumentBuilder()
+            val document = documentBuilder.parse(inputStream)
+            val element = document.documentElement
+
+            val itemlist = element.getElementsByTagName("item")
+            var itemChildren: NodeList
+
+            var currentItem: Node
+            var currentChild: Node
+
+            val items = ArrayList<HashMap<String, String>>()
+            var currentMap: HashMap<String, String>?
+
+            var imgCount = 0
+
+            for (i in 0 until itemlist.length) {
+
+                currentItem = itemlist.item(i)
+                itemChildren = currentItem.childNodes
+
+                currentMap = HashMap()
+
+                for (j in 0 until itemChildren.length) {
+
+                    currentChild = itemChildren.item(j)
+
+                    if (currentChild.nodeName.equals("title", ignoreCase = true)) {
+                        // Log.d("Title", String.valueOf(currentChild.getTextContent()));
+                        currentMap["title"] = currentChild.textContent
+                    }
+
+                    if (currentChild.nodeName.equals("description", ignoreCase = true)) {
+                        // Log.d("description", String.valueOf(currentChild.getTextContent()));
+                        currentMap["description"] = currentChild.textContent
+                    }
+
+                    if (currentChild.nodeName.equals("pubDate", ignoreCase = true)) {
+                        // Log.d("Title", String.valueOf(currentChild.getTextContent()));
+                        currentMap["pubDate"] = currentChild.textContent
+                    }
+
+                    if (currentChild.nodeName.equals("feedburner:origLink", ignoreCase = true)) {
+                        currentMap["origLink"] = currentChild.textContent
+                    }
+
+                    if (currentChild.nodeName.equals("media:thumbnail", ignoreCase = true)) {
+                        imgCount++
+
+                        if (imgCount == 1) {
+                            currentMap["imageUrl"] = currentChild.attributes.item(0).textContent
+                        }
+                    }
+                }
+                if (currentMap != null && !currentMap.isEmpty()) {
+                    items.add(currentMap)
+                }
+                imgCount = 0
+            }
+
+            return items
+
+        }
+    }
+
+    private fun showProgressDialog() {
+        isLoading = true
+        //progressDialog = ProgressBar(this)//ProgressDialog(this)
+        //progressDialog.setCancelable(false)
+        //progressDialog.setMessage("Fetching data from web..")
+        //progressDialog.show()
+    }
+
+    private fun hideProgressDialog() {
+        isLoading = false
+        /*if (progressDialog != null) {
+            progressDialog.cancel()
+        }*/
+    }
+
+    private fun getDataFromWeb() {
+        val fetchRss = GetRssClass(mFeedUrl)
+        fetchRss.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ GoodNewsEveryone/app/src/main/java/com/marvinboots/goodnewseveryone/RecycleViewAdapter.kt
@@ -0,0 +1,151 @@
+package com.marvinboots.goodnewseveryone
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.text.Html
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+import java.util.HashMap
+import androidx.cardview.widget.CardView
+import com.squareup.picasso.Picasso
+
+class RecycleViewAdapter( context: Context,  dataset : ArrayList<HashMap<String, String>>) : RecyclerView.Adapter<RecycleViewAdapter.ViewHolder>() {
+    private var mDataset = ArrayList<HashMap<String, String>>()
+    lateinit var mContext: Context
+    private var lastPosition = -1
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        var tv_title: TextView
+        var tv_category: TextView
+        var tv_description: TextView
+        var tv_date: TextView
+        var iv_image: ImageView
+        var iv_fav: ImageView
+        var iv_share: ImageView
+        var cardView: CardView
+
+
+        init {
+            tv_title = v.findViewById(R.id.title) as TextView
+            tv_category = v.findViewById(R.id.category) as TextView
+            tv_description = v.findViewById(R.id.description) as TextView
+            tv_date = v.findViewById(R.id.date) as TextView
+
+            iv_image = v.findViewById(R.id.image) as ImageView
+            iv_fav = v.findViewById(R.id.fav) as ImageView
+            iv_share = v.findViewById(R.id.share) as ImageView
+
+            cardView = v.findViewById(R.id.card_view) as CardView
+
+        }
+    }
+
+    init{
+        mContext = context
+        mDataset = dataset
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecycleViewAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.listitem, parent, false)
+
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // position should never be final
+
+        /*val animation = AnimationUtils.loadAnimation(
+            mContext,
+            if (position > lastPosition)
+                R.anim.up_from_bottom
+            else
+                R.anim.down_from_top
+        )
+        holder.itemView.startAnimation(animation)*/
+        lastPosition = position
+
+        val map = mDataset[position]
+
+        val imageurl = map["imageUrl"]
+
+        Picasso.get()//with(mContext)
+            .load(imageurl)
+            .placeholder(R.mipmap.placeholde)
+            .resize(dpToPx(128), dpToPx(128))
+            .centerInside()
+            .error(R.mipmap.placeholde)
+            .into(holder.iv_image)
+
+        holder.tv_title.text = map["title"]
+
+        val fromHtml = Html.fromHtml(map["description"]).toString()
+        val lastStr = fromHtml.substring(2, fromHtml.length - 1)
+
+        holder.tv_description.text = lastStr.trim { it <= ' ' }
+        holder.tv_date.text = map["pubDate"]
+
+
+        holder.cardView.setOnClickListener(onCardClick(holder.getAdapterPosition()))
+
+        holder.iv_share.setOnClickListener(onPostShare(holder.getAdapterPosition()))
+
+        holder.iv_fav.setOnClickListener(onAddfav(holder.getAdapterPosition()))
+
+    }
+
+
+    internal inner class onCardClick(var position: Int) : View.OnClickListener {
+
+        override fun onClick(view: View) {
+            val map = mDataset[position]
+            Log.d("Position", map["origLink"])
+            val uri = Uri.parse(map["origLink"])
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            mContext.startActivity(intent)
+        }
+    }
+
+    internal inner class onPostShare(var position: Int) : View.OnClickListener {
+
+        override fun onClick(view: View) {
+            val map = mDataset[position]
+            val sendIntent = Intent()
+            sendIntent.action = Intent.ACTION_SEND
+            sendIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                map["title"] + " \n" + map["origLink"]
+            )
+            sendIntent.type = "text/plain"
+            mContext.startActivity(Intent.createChooser(sendIntent, "Share this post"))
+        }
+    }
+
+    internal inner class onAddfav(var position: Int) : View.OnClickListener {
+        override fun onClick(view: View) {
+            Toast.makeText(mContext, "Added to favorites", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    override fun getItemCount(): Int {
+        return mDataset.size
+    }
+
+
+    fun dpToPx(dp: Int): Int {
+        val displayMetrics = mContext.resources
+            .displayMetrics
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT))
+    }
+}
